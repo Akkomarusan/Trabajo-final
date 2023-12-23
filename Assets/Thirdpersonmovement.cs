7d62396701123150209c5571a9cde5793c7487d5
Akkomarusan <149883601+Akkomarusan@users.noreply.github.com>
2023-12-23 09:54:47 -0300

    otra vez subiendo por si acaso
    .

--- Assets/Thirdpersonmovement.cs
+++ Assets/Thirdpersonmovement.cs
@@ -7,6 +7,15 @@ public class Thirdpersonmovement : MonoBehaviour
 
     public CharacterController controller;
     public Transform cam;
+    public Rigidbody rb;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
 
     public float speed = 6f;
 
@@ -14,12 +23,22 @@ public class Thirdpersonmovement : MonoBehaviour
 
     float turnSmoothVelocity;
 
+    Vector3 velocity;
+    bool isGrounded;
+
 
 
 
     // Update is called once per frame
     void Update()
     {
+
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -33,6 +52,11 @@ public class Thirdpersonmovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 }
